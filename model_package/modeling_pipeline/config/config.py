4fee6f5db5937547b6352ed7982573c86d2d669d
sa757 <kim850414@gmail.com>
2020-01-03 11:28:35 +0900

    restart16_1

--- model_package/modeling_pipeline/config/config.py
+++ model_package/modeling_pipeline/config/config.py
@@ -13,17 +13,10 @@ PACKAGE_ROOT = pathlib.Path(modeling_pipeline.__file__).resolve().parent
 TRAINED_MODEL_DIR = PACKAGE_ROOT / 'trained_models'
 DATASET_DIR = PACKAGE_ROOT / 'datasets'
 
-"""
-import zipfile
-with zipfile.ZipFile(DATASET_DIR / 'house-prices-advanced-regression-techniques.zip', 'r') as zip_ref:
-    zip_ref.extractall(DATASET_DIR)
-"""
-
 # data
 TESTING_DATA_FILE = 'test.csv'
 TRAINING_DATA_FILE = 'train.csv'
 TARGET = 'SalePrice'
-
 
 # variables
 FEATURES = ['MSSubClass', 'MSZoning', 'Neighborhood',
